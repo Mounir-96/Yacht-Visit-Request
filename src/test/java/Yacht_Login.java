d7b5b346af4e004f51618e48da74c06689035a6f
Mounir Sakr <113561308+Mounir-96@users.noreply.github.com>
2024-04-16 15:07:51 +0200

    Update Yacht_Login.java

--- src/test/java/Yacht_Login.java
+++ src/test/java/Yacht_Login.java
@@ -2,7 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Point;
-
 import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 
