8f7b3def6e2a44e811bafce2e18405d7efcf0c97
Mounir Sakr <113561308+Mounir-96@users.noreply.github.com>
2024-04-16 14:14:52 +0200

    Update Yacht_Login.java

--- src/test/java/Yacht_Login.java
+++ src/test/java/Yacht_Login.java
@@ -25,22 +25,23 @@ public class Yacht_Login {
         Thread.sleep(1000);
 
         // Access Yacht Single Window
-        WebElement Element = driver.findElement(By.xpath("//*[@id=\"menuform:j_idt152_12\"]/a/i[2]"));
+        WebElement Element = driver.findElement(By.xpath("//*[@id=\"menuform:j_idt152_12\"]/a/span"));
         js.executeScript("arguments[0].scrollIntoView();", Element);
         Element.click();
         Thread.sleep(1000);
-        WebElement Nafeza = driver.findElement(By.xpath("//*[@id=\"menuform:j_idt152_12_2\"]/a/i[2]"));
-        js.executeScript("arguments[0].scrollIntoView();", Nafeza);
-        Nafeza.click();
+        WebElement window = driver.findElement(By.xpath("//*[@id=\"menuform:j_idt152_12_2\"]/a/i[2]"));
+        js.executeScript("arguments[0].scrollIntoView();", window);
+        Thread.sleep(1000);
+        window.click();
         driver.findElement(By.xpath("//*[@id=\"menuform:j_idt152_12_2_0\"]/a")).click();
         Thread.sleep(2000);
 
 
         // Add button
-        WebElement Talab = driver.findElement(By.xpath("//*[@id=\"searchForm:compAdd:addEditButtonId\"]"));
+        WebElement Talab = driver.findElement(By.xpath("//*[@id=\"searchForm:fromEtdDate\"]/button"));
         js.executeScript("arguments[0].scrollIntoView();", Talab);
         Thread.sleep(1500);
-        Talab.click();
+        driver.findElement(By.xpath("//*[@id=\"searchForm:compAdd:addEditButtonId\"]")).click();
         Thread.sleep(1000);
 
         // Imo ( Seven numbers ) Generation
@@ -60,8 +61,8 @@ public class Yacht_Login {
         Thread.sleep(500);
         driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:countries_1\"]")).click();
         Thread.sleep(500);
-        WebElement hmoola = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:nrt_input\"]"));
-        hmoola.sendKeys("1000");
+        WebElement load = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:nrt_input\"]"));
+        load.sendKeys("1000");
         Thread.sleep(500);
         driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:netCapacity_input\"]")).sendKeys("900");
         Thread.sleep(500);
@@ -71,30 +72,30 @@ public class Yacht_Login {
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:depthMt_input\"]")).sendKeys("300");
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:depthFt_input\"]")).click();
+        WebElement tab = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:depthFt_input\"]"));
+        js.executeScript("arguments[0].scrollIntoView();", tab);
         Thread.sleep(1000);
-        WebElement molak = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt226:yachtOwnersBtn\"]"));
-        molak.click();
+        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt226:yachtOwnersBtn\"]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt226:dataTableId_data\"]/tr[1]/td[1]")).click();
         Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt224:dataTableId_data\"]/tr[1]/td[1]")).click();
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt224:dataTableId:selectBtn\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt226:dataTableId:selectBtn\"]")).click();
         Thread.sleep(2000);
         WebElement Visit_Data = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:YachtVisitPanel_header\"]/span"));
         js.executeScript("arguments[0].scrollIntoView()", Visit_Data);
 
 
         // Visit Reason
-        WebElement sbab = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:visitReasonId_label\"]"));
-        sbab.click();
+        WebElement reason = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:visitReasonId_label\"]"));
+        reason.click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:visitReasonId_3\"]")).click();
         Thread.sleep(500);
 
         // Radio Button
-        WebElement zaer = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:visitor\"]/div[2]/span"));
-        js.executeScript("arguments[0].scrollIntoView();", zaer);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", zaer);
+        WebElement visitor = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:visitor\"]/div[2]/span"));
+        js.executeScript("arguments[0].scrollIntoView();", visitor);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", visitor);
 
         // Expected arrival
 
@@ -109,18 +110,21 @@ public class Yacht_Login {
         driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:isArea1\"]/div[2]/span")).click();
 
         // Agency
-        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:shippingAgentId_label\"]")).click();
+      /*  WebElement tawkeel = driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:j_idt292\"]"));
+        js.executeScript("arguments[0].scrollIntoView()", tawkeel);
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:shippingAgentId_4\"]")).click();
-        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:shippingAgentId\"]/div[3]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:shippingAgentId_1\"]")).click();
+        Thread.sleep(2000);*/
 
         // Past Ports
         String winHandleBefore = driver.getWindowHandle();
-        driver.findElement(By.id("mainForm:tabs:dlg1")).click();
-        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@id=\"mainForm:tabs:dlg1\"]")).click();
+        Thread.sleep(1000);
         driver.switchTo().frame(0);
         Thread.sleep(500);
-        driver.findElement(By.xpath("  //*[@id=\"dlgSearchform:dataTableId_data\"]/tr[1]/td[1]")).click();
+        driver.findElement(By.xpath("  //*[@id=\"dlgSearchform:dataTableId_data\"]/tr[1]")).click();
         Thread.sleep(500);
         driver.findElement(By.id("dlgSearchform:dataTableId:selectBtn")).click();
         Thread.sleep(500);
@@ -169,9 +173,9 @@ public class Yacht_Login {
         Thread.sleep(500);
         driver.findElement(By.xpath("//*[@id=\"attchementComposite:addEditAttachmentForm:documentName_1\"]")).click();
         Thread.sleep(500);
-        WebElement raf3 = driver.findElement(By.name("attchementComposite:addEditAttachmentForm:fileId_input"));
+        WebElement upload = driver.findElement(By.name("attchementComposite:addEditAttachmentForm:fileId_input"));
         Thread.sleep(500);
-        raf3.sendKeys("C:\\Users\\mmaged\\Desktop\\1.png");
+        upload.sendKeys("C:\\Users\\mmaged\\Desktop\\1.png");
         Thread.sleep(1000);
         driver.findElement(By.id("attchementComposite:addEditAttachmentForm:j_idt27:saveButtonId")).click();
         Thread.sleep(800);
